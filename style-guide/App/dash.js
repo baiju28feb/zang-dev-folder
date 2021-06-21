e1565da635de682c535f05a3429f71786b7b6cc8
baiju <baiju28feb@gmail.com>
2021-06-22 00:25:02 +0530

    Hero section

--- style-guide/App/dash.js
+++ style-guide/App/dash.js
@@ -10,3 +10,45 @@ function togglenavbarActive(element) {
   previouslyClickedNavLink = element;
   element.classList.add('Active');
 }
+
+//chart start
+
+
+// const labels = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+// ];
+// const data = {
+//   labels: labels,
+//   datasets: [{
+//     label: 'My First dataset',
+//     backgroundColor: 'rgb(255, 99, 132)',
+//     borderColor: 'rgb(255, 99, 132)',
+//     data: [0, 10, 5, 2, 20, 30, 45],
+//   }]
+// };
+// // </block:setup>
+
+// // <block:config:0>
+// const config = {
+//   type: 'line',
+//   data,
+//   options: {}
+// };
+// // </block:config>
+
+// module.exports = {
+//   actions: [],
+//   config: config,
+// };
+
+// var myChart = new Chart(
+//   document.getElementById('myChart'),
+//   config
+// );
+
+//chart end
