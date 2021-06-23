1e1baaebeda16593ab2acfd662c224fa57c9c842
baiju <baiju28feb@gmail.com>
2021-06-23 08:45:30 +0530

    dash icons

--- style-guide/App/dash.js
+++ style-guide/App/dash.js
@@ -14,41 +14,36 @@ function togglenavbarActive(element) {
 //chart start
 
 
-// const labels = [
-//   'January',
-//   'February',
-//   'March',
-//   'April',
-//   'May',
-//   'June',
-// ];
-// const data = {
-//   labels: labels,
-//   datasets: [{
-//     label: 'My First dataset',
-//     backgroundColor: 'rgb(255, 99, 132)',
-//     borderColor: 'rgb(255, 99, 132)',
-//     data: [0, 10, 5, 2, 20, 30, 45],
-//   }]
-// };
-// // </block:setup>
-
-// // <block:config:0>
-// const config = {
-//   type: 'line',
-//   data,
-//   options: {}
-// };
-// // </block:config>
-
-// module.exports = {
-//   actions: [],
-//   config: config,
-// };
-
-// var myChart = new Chart(
-//   document.getElementById('myChart'),
-//   config
-// );
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Slack', 'G-Calender', 'M-teams', 'G-Meet', 'Zoom', 'Others'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      indexAxis: 'y',
+    }
+});
 
 //chart end
